d1d3979801d4721ec05d702c9ab29c25c7959adc
justdogbu <giaminhro1611@gmail.com>
2023-04-26 14:58:38 +0700

    Login UI

--- AccuPhone/Form1.Designer.cs
+++ AccuPhone/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AccuPhone
 {
-    partial class Form1
+    partial class fLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserEmail = new System.Windows.Forms.TextBox();
+            this.panelUser = new System.Windows.Forms.Panel();
+            this.panelPassword = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.bLogin = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::GUI.Properties.Resources.accounting;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 130);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 311);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Comfortaa", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(107, 40);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(226, 36);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "ACCOUNT LOGIN";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comfortaa", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(64, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "make sure your account is secure";
+            // 
+            // txtUserEmail
+            // 
+            this.txtUserEmail.BackColor = System.Drawing.SystemColors.Control;
+            this.txtUserEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserEmail.ForeColor = System.Drawing.Color.DimGray;
+            this.txtUserEmail.Location = new System.Drawing.Point(443, 203);
+            this.txtUserEmail.Multiline = true;
+            this.txtUserEmail.Name = "txtUserEmail";
+            this.txtUserEmail.Size = new System.Drawing.Size(303, 47);
+            this.txtUserEmail.TabIndex = 3;
+            this.txtUserEmail.Text = "Type your email";
+            this.txtUserEmail.Click += new System.EventHandler(this.txtUserEmail_Click);
+            this.txtUserEmail.Leave += new System.EventHandler(this.txtUserEmail_Leave);
+            // 
+            // panelUser
+            // 
+            this.panelUser.BackColor = System.Drawing.Color.Silver;
+            this.panelUser.ForeColor = System.Drawing.Color.Coral;
+            this.panelUser.Location = new System.Drawing.Point(443, 235);
+            this.panelUser.Name = "panelUser";
+            this.panelUser.Size = new System.Drawing.Size(303, 5);
+            this.panelUser.TabIndex = 5;
+            // 
+            // panelPassword
+            // 
+            this.panelPassword.BackColor = System.Drawing.Color.Silver;
+            this.panelPassword.ForeColor = System.Drawing.Color.Coral;
+            this.panelPassword.Location = new System.Drawing.Point(443, 302);
+            this.panelPassword.Name = "panelPassword";
+            this.panelPassword.Size = new System.Drawing.Size(303, 5);
+            this.panelPassword.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.ForeColor = System.Drawing.Color.DimGray;
+            this.txtPassword.Location = new System.Drawing.Point(443, 270);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(303, 47);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.Text = "Type your password";
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.Click += new System.EventHandler(this.txtPassword_Click);
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // bLogin
+            // 
+            this.bLogin.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.bLogin.Font = new System.Drawing.Font("Comfortaa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bLogin.Location = new System.Drawing.Point(443, 346);
+            this.bLogin.Name = "bLogin";
+            this.bLogin.Size = new System.Drawing.Size(303, 47);
+            this.bLogin.TabIndex = 8;
+            this.bLogin.Text = "LOGIN";
+            this.bLogin.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::GUI.Properties.Resources.icon_account_5_removebg_preview;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = global::GUI.Properties.Resources.icon_account_5_removebg_preview;
+            this.pictureBox2.Location = new System.Drawing.Point(716, 203);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 26);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::GUI.Properties.Resources.lock_removebg_preview;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(716, 263);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(29, 33);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // fLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.bLogin);
+            this.Controls.Add(this.panelPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.panelUser);
+            this.Controls.Add(this.txtUserEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fLogin";
+            this.ShowInTaskbar = false;
+            this.Text = "AccuPhone";
+            this.Load += new System.EventHandler(this.fLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label lblTitle;
+        private Label label1;
+        private TextBox txtUserEmail;
+        private Panel panelUser;
+        private Panel panelPassword;
+        private TextBox txtPassword;
+        private Button bLogin;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
     }
 }
--- AccuPhone/Form1.cs
+++ AccuPhone/Form1.cs
@@ -1,10 +1,64 @@
 namespace AccuPhone
 {
-    public partial class Form1 : Form
+    public partial class fLogin : Form
     {
-        public Form1()
+        public fLogin()
         {
             InitializeComponent();
+        }
+
+        private void fLogin_Load(object sender, EventArgs e)
+        {
+            bLogin.TabStop = false;
+            bLogin.FlatStyle = FlatStyle.Flat;
+            bLogin.FlatAppearance.BorderSize = 0;
+            bLogin.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+        }
+
+  
+        
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            panelPassword.BackColor = Color.Silver;
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Type your password";
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            panelPassword.BackColor = Color.DeepSkyBlue;
+            if (txtPassword.Text == "Type your password")
+            {
+                txtPassword.Text = string.Empty;
+            }
+        }
+
+        private void txtUserEmail_Click(object sender, EventArgs e)
+        {
+            panelUser.BackColor = Color.DeepSkyBlue;
+            if (txtUserEmail.Text == "Type your email")
+            {
+                txtUserEmail.Text = string.Empty;
+            }
+        }
+
+        private void txtUserEmail_Leave(object sender, EventArgs e)
+        {
+            panelUser.BackColor = Color.Silver;
+            if (txtUserEmail.Text == "")
+            {
+                txtUserEmail.Text = "Type your email";
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = false;
         }
     }
 }
